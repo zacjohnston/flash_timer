b4a8f0137de4a35a388a6b6efc8675298a7cef82
Zac Johnston <zacpjohnston@gmail.com>
2020-10-19 17:29:28 -0400

    [flash_timer.py] added get_summary_line()

--- flash_timer/flash_timer.py
+++ flash_timer/flash_timer.py
@@ -2,6 +2,29 @@ import numpy as np
 import pandas as pd
 
 
+def get_summary_line(filepath):
+    """Extract line number of perf summary from .log file
+    Return: pd.DataFrame
+
+    parameters
+    ----------
+    filepath : str
+        path to .log file
+    """
+    lines = read_log(filepath=filepath)
+    n = None
+
+    for i, line in enumerate(lines):
+        if line == ' perf_summary: code performance summary statistics':
+            n = i
+            break
+
+    if n is None:
+        print(f'No summary found in log file: {filepath}')
+        
+    return n
+
+
 def read_log(filepath):
     """Load .log file as list of lines
 
