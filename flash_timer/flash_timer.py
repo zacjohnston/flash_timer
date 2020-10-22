39139ab02a1cef8d5cbf439c2798d3754b8f25ae
Zac Johnston <zacpjohnston@gmail.com>
2020-10-22 16:12:12 -0400

    [flash_timer.py] added extract_table()

--- flash_timer/flash_timer.py
+++ flash_timer/flash_timer.py
@@ -2,6 +2,30 @@ import numpy as np
 import pandas as pd
 
 
+def extract_table(filepath):
+    """Get perf timing table from .log file
+
+    Return: pd.DataFrame
+
+    parameters
+    ----------
+    filepath : str
+        path to .log file
+    """
+    offset = 19  # line offset of evolution from line_0
+    line_0 = get_summary_line(filepath)
+
+    table = pd.read_csv(filepath, skiprows=line_0+offset, skipfooter=2,
+                        header=None, sep=r"[ ]{2,}", engine='python')
+
+    table.set_index(0, inplace=True)
+    # table = table.transpose()
+    table.columns = ['max', 'min', 'avg', 'calls']
+    table.index.rename('unit', inplace=True)
+
+    return table
+
+
 def get_evolution(filepath):
     """Get evolution time from .log file
 
@@ -32,17 +56,20 @@ def get_summary_line(filepath):
         path to .log file
     """
     lines = read_log(filepath=filepath)
-    line_0 = None
+    line_0 = []
 
     for i, line in enumerate(lines):
         if line == ' perf_summary: code performance summary statistics':
-            line_0 = i
-            break
+            line_0 += [i]
 
-    if line_0 is None:
-        print(f'No summary found in log file: {filepath}')
-
-    return line_0
+    if len(line_0) is 0:
+        print(f'No performance summary found in log file: {filepath}')
+        return None
+    elif len(line_0) > 1:
+        print(f'WARNING: multiple runs found in log file. Returning most recent only')
+        return line_0[-1]
+    else:
+        return line_0[0]
 
 
 def read_log(filepath):
