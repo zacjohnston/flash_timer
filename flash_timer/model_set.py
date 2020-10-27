594ded2679db11e50eb77ff58e35e24700d72f8c
Zac Johnston <zacpjohnston@gmail.com>
2020-10-27 18:32:02 -0400

    [model_set.py] changed omp_threads to just int

--- flash_timer/model_set.py
+++ flash_timer/model_set.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # flash_timer
 from . import model
-from .tools import ensure_sequence
+from . import tools
 
 
 class ModelSet:
@@ -21,41 +21,47 @@ class ModelSet:
         ----------
         model_set : str
             name of model set/collection
+        omp_threads : int
+            OpenMP threads used
         leaf_blocks : [int]
             list of total leaf blocks
-        omp_threads : [int]
-            list of OpenMP threads used
         mpi_ranks : [int]
             list of MPI ranks used
         log_basename : str
             Prefix used in logfile name, e.g. 'sod3d' for 'sod3d_16.log`
         """
         self.model_set = model_set
-        self.omp_threads = ensure_sequence(omp_threads)
-        self.leaf_blocks = ensure_sequence(leaf_blocks)
-        self.mpi_ranks = ensure_sequence(mpi_ranks)
+
+        self.omp_threads = omp_threads
+        self.leaf_blocks = leaf_blocks
+        self.mpi_ranks = mpi_ranks
+        self.expand_sequences()
 
         self.models = {}
 
-        for threads in self.omp_threads:
-            self.models[threads] = {}
+        for leafs in self.leaf_blocks:
+            self.models[leafs] = {}
 
-            for leafs in self.leaf_blocks:
-                self.models[threads][leafs] = {}
+            for ranks in self.mpi_ranks:
+                self.models[leafs][ranks] = model.Model(model_set=model_set,
+                                                        omp_threads=self.omp_threads,
+                                                        leaf_blocks=leafs,
+                                                        mpi_ranks=ranks,
+                                                        log_basename=log_basename)
 
-                for ranks in self.mpi_ranks:
-                    self.models[threads][leafs][ranks] = model.Model(
-                                                            model_set=model_set,
-                                                            omp_threads=threads,
-                                                            leaf_blocks=leafs,
-                                                            mpi_ranks=ranks,
-                                                            log_basename=log_basename)
+    def expand_sequences(self):
+        """Expand sequence attributes
+        """
+        self.leaf_blocks = tools.ensure_sequence(self.leaf_blocks)
 
-    def get_times(self, unit, omp_threads, leaf_blocks):
+        if not isinstance(self.mpi_ranks, (list, tuple, np.ndarray)):
+            self.mpi_ranks = tools.expand_power_sequence(largest=self.mpi_ranks)
+
+    def get_times(self, unit, leaf_blocks):
         """Return array of times versus mpi_ranks
         """
         times = []
-        models = self.models[omp_threads][leaf_blocks]
+        models = self.models[leaf_blocks]
 
         for i, m in models.items():
             t = m.table.loc[unit, 'avg']
@@ -64,41 +70,37 @@ class ModelSet:
 
         return np.array(times)
 
-    def plot_strong_speedup(self, omp_threads, unit='evolution'):
+    def plot_strong_speedup(self, unit='evolution'):
         """Plot scaling
         """
         fig, ax = plt.subplots()
 
         for leafs in self.leaf_blocks:
-            times = self.get_times(unit=unit, omp_threads=omp_threads,
-                                   leaf_blocks=leafs)
+            times = self.get_times(unit=unit, leaf_blocks=leafs)
             ax.plot(self.mpi_ranks, times[0]/times, marker='o', label=leafs)
 
         last_rank = self.mpi_ranks[-1]
         ax.plot([0, last_rank], [0, last_rank], ls='--', color='black')
 
-        self._set_ax(ax=ax, omp_threads=omp_threads, x_label='MPI ranks',
-                     y_label='Speedup')
+        self._set_ax(ax=ax, x_label='MPI ranks', y_label='Speedup')
 
-    def plot_strong_time(self, omp_threads, unit='evolution'):
+    def plot_strong_time(self, unit='evolution'):
         """Plot scaling
         """
         fig, ax = plt.subplots()
 
         for leafs in self.leaf_blocks:
-            times = self.get_times(unit=unit, omp_threads=omp_threads,
-                                   leaf_blocks=leafs)
+            times = self.get_times(unit=unit, leaf_blocks=leafs)
             ax.plot(self.mpi_ranks, times, marker='o', label=leafs)
 
-        self._set_ax(ax=ax, omp_threads=omp_threads, x_label='MPI ranks',
-                     y_label='Time (s)')
+        self._set_ax(ax=ax, x_label='MPI ranks', y_label='Time (s)')
 
-    def _set_ax(self, ax, omp_threads, x_label, y_label):
+    def _set_ax(self, ax, x_label, y_label):
         """Set axis properties
         """
         ax.legend(title='Leaf blocks')
         ax.set_xlabel(x_label)
         ax.set_ylabel(y_label)
-        ax.set_title(f'{self.model_set}, OMP_THREADS={omp_threads}')
+        ax.set_title(f'{self.model_set}, OMP_THREADS={self.omp_threads}')
 
 
