4a4cf9f8b24dbb7ff704eba53fe262bae8c5fa50
Zac Johnston <zacpjohnston@gmail.com>
2020-10-27 17:44:12 -0400

    [model_set.py] added plot methods

--- flash_timer/model_set.py
+++ flash_timer/model_set.py
@@ -1,4 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# flash_timer
 from . import model
+from .tools import ensure_sequence
 
 
 class ModelSet:
@@ -9,7 +14,7 @@ class ModelSet:
                  omp_threads,
                  leaf_blocks,
                  mpi_ranks,
-                 log_filepath=None):
+                 log_basename='sod3d'):
         """
 
         parameters
@@ -22,27 +27,76 @@ class ModelSet:
             list of OpenMP threads used
         mpi_ranks : [int]
             list of MPI ranks used
-        log_filepath : str
-            path to .log file (optional)
+        log_basename : str
+            Prefix used in logfile name, e.g. 'sod3d' for 'sod3d_16.log`
         """
         self.model_set = model_set
-        self.omp_threads = omp_threads
-        self.leaf_blocks = leaf_blocks
-        self.mpi_ranks = mpi_ranks
+        self.omp_threads = ensure_sequence(omp_threads)
+        self.leaf_blocks = ensure_sequence(leaf_blocks)
+        self.mpi_ranks = ensure_sequence(mpi_ranks)
 
         self.models = {}
 
         for threads in self.omp_threads:
             self.models[threads] = {}
 
-            for leaf in self.leaf_blocks:
-                self.models[threads][leaf] = {}
+            for leafs in self.leaf_blocks:
+                self.models[threads][leafs] = {}
 
-                for rank in self.mpi_ranks:
-                    self.models[threads][leaf][rank] = model.Model(model_set=model_set,
-                                                                   omp_threads=threads,
-                                                                   leaf_blocks=leaf,
-                                                                   mpi_ranks=rank)
+                for ranks in self.mpi_ranks:
+                    self.models[threads][leafs][ranks] = model.Model(
+                                                            model_set=model_set,
+                                                            omp_threads=threads,
+                                                            leaf_blocks=leafs,
+                                                            mpi_ranks=ranks,
+                                                            log_basename=log_basename)
+
+    def get_times(self, unit, omp_threads, leaf_blocks):
+        """Return array of times versus mpi_ranks
+        """
+        times = []
+        models = self.models[omp_threads][leaf_blocks]
+
+        for i, m in models.items():
+            t = m.table.loc[unit, 'avg']
+            # t = m.table.loc[unit, 'avg'][1]
+            times += [t]
+
+        return np.array(times)
+
+    def plot_strong_speedup(self, omp_threads, unit='evolution'):
+        """Plot scaling
+        """
+        fig, ax = plt.subplots()
+
+        for leafs in self.leaf_blocks:
+            times = self.get_times(unit=unit, omp_threads=omp_threads,
+                                   leaf_blocks=leafs)
+            ax.plot(self.mpi_ranks, times[0]/times, marker='o', label=leafs)
+
+        last_rank = self.mpi_ranks[-1]
+        ax.plot([0, last_rank], [0, last_rank], ls='--', color='black')
+
+        ax.legend(title='Leaf blocks')
+        ax.set_xlabel('MPI ranks')
+        ax.set_ylabel('Speedup')
+        ax.set_title(f'{self.model_set}, OMP_THREADS={omp_threads}')
+
+    def plot_strong_time(self, omp_threads, unit='evolution'):
+        """Plot scaling
+        """
+        fig, ax = plt.subplots()
+
+        for leafs in self.leaf_blocks:
+            times = self.get_times(unit=unit, omp_threads=omp_threads,
+                                   leaf_blocks=leafs)
+            ax.plot(self.mpi_ranks, times, marker='o', label=leafs)
+
+        ax.legend(title='Leaf blocks')
+        ax.set_xlabel('MPI ranks')
+        ax.set_ylabel('Time (s)')
+        ax.set_title(f'{self.model_set}, OMP_THREADS={omp_threads}')
+
 
 
 
